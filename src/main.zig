f584f3e54f02c3492f61cdbc6c4ee802d9561794
thomas <thomas.treffry@nearform.com>
2022-10-26 16:14:50 -0700

    feat: add filled rectangle to the window

--- src/main.zig
+++ src/main.zig
@@ -19,8 +19,20 @@ pub fn main() !void {
     defer renderer.destroy();
 
     while (true) {
-        try renderer.setColorRGBA(0, 0, 0, 0);
-        try renderer.clear();
+        try renderer.setColorRGBA(0, 0, 0, 0); // set color to black
+        try renderer.clear(); // clear the screen with color
+
+        // 40x40 rectangle
+        const r = sdl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .width = 40,
+            .height = 40,
+        };
+
+        try renderer.setColorRGBA(255, 255, 255, 0); // set color to white
+        try renderer.fillRect(r); // fill rectangle with color
+        renderer.present();
     }
 
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
