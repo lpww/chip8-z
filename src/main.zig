b3e66549b845fa2adac3a0d8a3c764fd3c3b6f31
thomas <thomas.treffry@nearform.com>
2022-10-26 15:08:15 -0700

    feat: render a black screen

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,24 @@ const std = @import("std");
 const sdl = @import("sdl2");
 
 pub fn main() !void {
-    const init_code = sdl.SDL_Init(sdl.SDL_INIT_EVERYTHING);
+    trySdl(sdl.SDL_Init(sdl.SDL_INIT_EVERYTHING));
     defer sdl.SDL_Quit();
-    checkSdlErr(init_code);
 
     const window = sdl.SDL_CreateWindow("chip8-z window", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, 640, 320, sdl.SDL_WINDOW_SHOWN) orelse panic();
     defer sdl.SDL_DestroyWindow(window);
 
-    while (true) {}
+    const renderer = sdl.SDL_CreateRenderer(window, -1, sdl.SDL_TEXTUREACCESS_TARGET);
+    defer sdl.SDL_DestroyRenderer(renderer);
+
+    while (true) {
+        trySdl(sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0)); // black
+        trySdl(sdl.SDL_RenderClear(renderer)); // clear the screen with renderer color
+    }
 
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
 
-fn checkSdlErr(code: c_int) void {
+fn trySdl(code: c_int) void {
     if (code < 0) panic();
 }
 
