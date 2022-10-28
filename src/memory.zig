cb80a76faedd1cf1f0d25f51fffa26afd29eecbc
thomas <thomas.treffry@nearform.com>
2022-10-28 12:17:41 -0700

    feat: add memory out of bounds check

--- src/memory.zig
+++ src/memory.zig
@@ -5,11 +5,17 @@ const CHIP8_MEMORY_SIZE = @import("config.zig").CHIP8_MEMORY_SIZE;
 pub const Memory = struct {
     data: [CHIP8_MEMORY_SIZE]u8 = mem.zeroes([CHIP8_MEMORY_SIZE]u8),
 
+    fn indexOutOfBounds(index: u16) bool {
+        return index < 0 and index < CHIP8_MEMORY_SIZE;
+    }
+
     pub fn get(self: *Memory, index: u16) u8 {
+        if (indexOutOfBounds(index)) @panic("out of bounds");
         return self.data[index];
     }
 
     pub fn set(self: *Memory, index: u16, value: u8) !void {
+        if (indexOutOfBounds(index)) @panic("out of bounds");
         self.data[index] = value;
     }
 };
