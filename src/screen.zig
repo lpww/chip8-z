9d307ff3ee56b92cb6771abc2224e2b9db97e9b5
thomas <thomas.treffry@nearform.com>
2023-01-09 18:15:26 -0800

    feat: implement sprite drawing

--- src/screen.zig
+++ src/screen.zig
@@ -29,4 +29,30 @@ pub const Screen = struct {
         if (isOutOfBounds(x, y)) @panic("screen is out of bounds");
         return self.pixels[y][x];
     }
+
+    pub fn drawSprite(self: *Screen, x: u16, y: u16, sprite: []const u8) bool {
+        const binary_int: u512 = 0b10000000; // made this huge to accomodate right hand bit shift on line 45, better way?
+
+        var pixel_collison = false;
+        var lx: u8 = 0;
+
+        for (sprite) |c, ly| {
+            lx = 0;
+            while (lx < 8) : (lx += 1) {
+                if ((c & (binary_int >> lx)) == 0) {
+                    continue;
+                }
+
+                const pixel_value: bool = self.pixels[(ly + y) % CHIP8_HEIGHT][(lx + x) % CHIP8_WIDTH];
+
+                if (pixel_value) {
+                    pixel_collison = true;
+                }
+
+                self.pixels[(ly + y) % CHIP8_HEIGHT][(lx + x) % CHIP8_WIDTH] = pixel_value != true;
+            }
+        }
+
+        return pixel_collison;
+    }
 };
